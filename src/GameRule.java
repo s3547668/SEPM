a9cdeb51b02517c10277fc0b90d485cdcae3ca45
s3547668 <87790878+s3547668@users.noreply.github.com>
2022-05-08 13:40:58 +1000

    Create GameRule.java

---
+++ src/GameRule.java
@@ -0,0 +1,14 @@
+public class GameRule {
+
+    public GameRule(){
+    }
+
+    public static void displayRules(){
+        System.out.println("Game Rules");
+        System.out.println("Start by inputting a five-letter word ad hitting enter. " +
+                "The letter wills turn to different colours or stay blank. " +
+                "If the colour doesn’t change and remains blank, the input letter is not in the word. " +
+                "If the colour has changed, it means that the letter is included in the word. ");
+
+    }
+}
